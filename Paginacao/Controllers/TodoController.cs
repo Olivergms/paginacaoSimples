a82f020937252db6d9c06b2e5cd600ae21d14358
Adriano Gomes de Oliveira <oliveradriano.ads@gmail.com>
2022-04-18 22:44:08 -0300

    Api com paginacao simples

---
+++ Paginacao/Controllers/TodoController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Paginacao.Data;
+using Paginacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paginacao.Controllers
+{
+    [ApiController]
+    [Route("v1/todos")]
+    public class TodoController : ControllerBase
+    {
+        [HttpGet("load")]
+        public async Task<IActionResult> LoadAsync([FromServices]AppDbContext context )
+        {
+            for (int i = 0; i < 1348; i++)
+            {
+                var todo = new Todo()
+                {
+                    Id = i + 1,
+                    Done = false,
+                    CreatedAt = DateTime.Now,
+                    Title = $"Tarefa {i}"
+                };
+
+                await context.Todos.AddAsync(todo);
+                await context.SaveChangesAsync();                
+            }
+
+            return Ok();
+        }
+
+        [HttpGet("skip/{skip:int}/take/{take:int}")]
+        public async Task<IActionResult> GetAsync([FromServices] AppDbContext context,
+            int skip = 0,
+            int take=25)
+        {
+
+            if (take > 1000) return BadRequest("Quantidade limitada a 1000 registros.");
+
+            var total = await context.Todos.CountAsync();
+            var todos = await context
+                .Todos
+                .AsNoTracking()
+                .Skip(skip)
+                .Take(take)
+                .ToListAsync();
+
+
+            return Ok(new 
+            {
+                total,
+                skip,
+                take,
+                data = todos 
+            });
+        }
+    }
+}
---
+++ Paginacao/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Paginacao.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paginacao.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
